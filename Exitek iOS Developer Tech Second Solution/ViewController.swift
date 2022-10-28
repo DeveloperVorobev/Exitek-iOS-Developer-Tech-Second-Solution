3df4a8e1cdaffd678a98ae5c84a1e992e55ff9a9
Vladislav Vorobev <vlad_imbo1994@mail.ru>
2022-10-28 20:40:17 +0500

    Initial Commit

---
+++ Exitek iOS Developer Tech Second Solution/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exitek iOS Developer Tech Second Solution
+//
+//  Created by Владислав Воробьев on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
